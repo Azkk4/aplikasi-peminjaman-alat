df0d62d2f7a4bed6b632ac970d236673e88feb44
Azkk4 <alazka.rezvan@gmail.com>
2026-07-29 11:38:18 +0700

    First Commit

---
+++ backend/routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\KategoriController;
+use App\Http\Controllers\API\AlatController;
+use App\Http\Controllers\API\UserController;
+
+// Public Routes (Tidak perlu token) 
+Route::post('/register', [AuthController::class, 'register']); 
+Route::post('/login', [AuthController::class, 'login']); 
+ 
+// Protected Routes (Wajib membawa Bearer Token dari Sanctum) 
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::get('/me', [AuthController::class, 'me']); 
+    Route::post('/logout', [AuthController::class, 'logout']); 
+ 
+    // ==================
+    // ADMIN
+    // ==================
+    Route::middleware('role.admin')->group(function () { 
+        Route::apiResource('kategori', KategoriController::class);
+
+        Route::apiResource('alat', AlatController::class);
+        Route::get('/katalog', [AlatController::class, 'katalog']);
+
+        Route::apiResource('users', UserController::class);
+    }); 
+    // ==================
+    // PETUGAS
+    // ==================
+
+    Route::middleware('role.petugas')->group(function () { 
+        // Route untuk hak akses petugas 
+        }); 
+
+    // ==================
+    // PEMINJAM
+     // =================
+    Route::middleware('role.peminjam')->group(function () { 
+    // Route untuk hak akses peminjam 
+    }); 
+});
