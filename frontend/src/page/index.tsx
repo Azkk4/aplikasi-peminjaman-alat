eb812c88d0337d7626c52c96ae829c2acfa69ab1
Azkk4 <alazka.rezvan@gmail.com>
2026-08-04 13:14:18 +0700

    Setup React frontend

---
+++ frontend/src/page/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container w-[1440px] h-[1024px] bg-[#a47449] relative overflow-hidden mx-auto my-0">
+      <div className="w-[1286px] h-[1032px] bg-[#809b79] rounded-[50px] border-solid border-[5px] border-[#5a7254] absolute top-[16px] left-[221px] z-[1]" />
+      <div className="w-[1286px] h-[1036px] bg-[#99c28e] rounded-[50px] border-solid border-[6px] border-[#677e61] absolute top-[34px] left-[203px]">
+        <div className="w-[371.522px] h-[522.719px] bg-[#a1f8ff] rounded-[25px] absolute top-[354.13px] left-[-6px] rotate-[-18.87deg] shadow-[4px_4px_4px_0_rgba(255,255,255,0.25)_inset] z-[5]" />
+        <div className="w-[488.137px] h-[506.512px] bg-[#75ebf3] rounded-[25px] absolute top-[472px] left-[-6px] rotate-[-41.93deg] shadow-[0_4px_4px_0_rgba(255,255,255,0.25)_inset] z-[4]" />
+      </div>
+      <div className="w-[1286px] h-[1032px] bg-[#d8ffcd] rounded-[50px] absolute top-[34px] left-[240px] z-[2]" />
+      <div className="w-[1205px] h-[1032px] bg-[#fffff2] rounded-[25px] absolute top-[47px] left-[278px] z-[3]" />
+      <div className="w-[1205px] h-[1032px] bg-[#ffffd4] rounded-[25px] absolute top-[66px] left-[316px] shadow-[-10px_4px_4px_0_rgba(0,0,0,0.15)] z-[8]">
+        <div className="w-[1088px] h-[995px] bg-[#f5f5f0] rounded-[5px] border-solid border-[6px] border-[#c57300] absolute top-[18px] left-[23px] z-[9]">
+          <div className="w-[54.575px] h-[54.575px] bg-[url(https://static.codia.ai/image/2026-08-03/f5aRWB7oJL.png)] bg-cover bg-no-repeat absolute top-[-7px] left-[-7px] z-[11]" />
+          <div className="w-[54.575px] h-[54.575px] bg-[url(https://static.codia.ai/image/2026-08-03/siBSr3JEi6.png)] bg-cover bg-no-repeat absolute top-[-7px] left-[1027px] z-[12]" />
+        </div>
+        <div className="w-[1050px] h-[978px] rounded-[1px] border-solid border-2 border-[rgba(197,115,0,0.5)] absolute top-[39px] left-[42px] box-content z-10" />
+      </div>
+      <div className="w-[324px] h-[98px] bg-[url(https://static.codia.ai/image/2026-08-03/e0y10WokyU.png)] bg-cover bg-no-repeat absolute top-[110px] left-[41px] z-[6]">
+        <span className="flex w-[168px] h-[66px] justify-start items-center font-['Joan'] text-[24px] font-normal leading-[31.008px] text-[#000] absolute top-[16px] left-[52px] text-left z-[13]">
+          Dashboard
+        </span>
+      </div>
+      <div className="w-[305px] h-[98px] bg-[url(https://static.codia.ai/image/2026-08-03/uO08KtzheW.png)] bg-cover bg-no-repeat absolute top-[217px] left-[60px] z-[7]">
+        <span className="flex w-[168px] h-[66px] justify-start items-center font-['Joan'] text-[24px] font-normal leading-[31.008px] text-[#000] absolute top-[16px] left-[33px] text-left z-[14]">
+          Kelola Alat
+        </span>
+      </div>
+    </div>
+  );
+}
